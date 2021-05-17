8c6df988c272fa7945b290d9d8207fcec9c10ffb
illionillion <yuto.0526.sano@gmail.com>
2021-05-17 23:26:16 +0900

    Reactでのif文でhtml切り替えたりmap、for文で配列から繰り返し表示とかした。phpの：と；使うifに似ているような

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@
 // import './App.css';
 import {useState} from 'react';
 import {List} from './List';
+import {Form} from './form';
 
 function App() {
-  const [description,setDescription]=useState('クリック前の表示');
-  const changeDescription=()=>{
-    setDescription('クリック後の表示です、');
-  }
+  // const [description,setDescription]=useState('クリック前の表示');
+  const [tab,setTab]=useState('list');
+  // const changeDescription=()=>{
+  //   setDescription('クリック後の表示です、');
+  // }
+  const body=tab==='list' ?  <List title="取扱言語一覧"/>: <Form/>;
   return (
     <div style={{color:'red',background:'blue'}}>
       {/* ゼロから始めるReact入門 */}
-      {description}
-      <List title="取扱言語一覧"/>
-      <button onClick={()=>changeDescription()}>ボタン</button>
+      <header>
+        <ul>
+          <li onClick={()=>setTab('list')}>リスト</li>
+          <li onClick={()=>setTab('form')}>フォーム</li>
+        </ul>
+      </header>
+      <hr/>
+      {/* {description} */}
+      {body}
+      {/* <button onClick={()=>changeDescription()}>ボタン</button> */}
     </div>
   );
 }
--- src/List.js
+++ src/List.js
@@ -1,10 +1,20 @@
-export const List = (props) => {
-  const title = props.title;
+const LANGUAGES=[
+  'JavaScript',
+  'PHP',
+  'VBA'
+];
+
+export const List = () => {
+  // const title = props.title;
   
   return(
     <div>
-      <h4 style={{color:'green',background:'yellow'}}>{title}</h4>
-      <div>リストです。</div>
+      {/* <h4 style={{color:'green',background:'yellow'}}>{title}</h4> */}
+      {
+        LANGUAGES.map((string,number)=>{
+          return <div key={number} style={{color:'green',background:'yellow'}}>{string}</div>
+        })
+      }
     </div>
   )
 }
